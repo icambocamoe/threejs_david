da3519b9cbaf328fe2cc7ef5b79dcc471f6c3705
icambocamoe <89269526+icambocamoe@users.noreply.github.com>
2022-02-19 13:26:24 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -31,3 +31,21 @@ pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
 
+
+
+var geometry = new THREE.BoxGeometry( 3, 3, 3);
+var material = new THREE.MeshBasicMaterial( {
+ color: "#dadada", wireframe: true, transparent: true
+});
+var wireframeCube = new THREE.Mesh ( geometry, material );
+scene.add( wireframeCube );
+
+function animate() {
+ requestAnimationFrame( animate );
+ cube.rotation.x += 0.04;
+ cube.rotation.y += 0.04;
+ wireframeCube.rotation.x -= 0.01;
+ wireframeCube.rotation.y -= 0.01;
+ renderer.render( scene, camera );
+}
+animate();
