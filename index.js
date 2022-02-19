86a436530e26d463ca28fa2a80e74eeceb78091c
icambocamoe <89269526+icambocamoe@users.noreply.github.com>
2022-02-19 12:43:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 const renderer = new THREE.WebGLRenderer({ antialias: true})
+
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.body.appendChild( renderer.domElement )
+
+document.body.appendChild( renderer.domElement )
+var geometry = new THREE.BoxGeometry( 1, 1, 1)
+var material = new THREE.MeshBasicMaterial( { color: 0xff0051 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
+renderer.render( scene, camera )camera.position.z = 5
